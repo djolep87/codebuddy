1fe19b38dfe598bbc99759994ce03378a72ba603
djolep <djolep987@gmail.com>
2020-10-16 19:00:00 +0200

    first page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,9 +15,37 @@
             </div>
 
             <div class="mt-4">
+                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
+
+            {{-- <div class="mt-4">
+                <x-jet-label for="name" value="{{ __('Expertise level') }}" />
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                    <label class="form-check-label" for="inlineRadio1">Entry level</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                    <label class="form-check-label" for="inlineRadio2">Intermediate</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                    <label class="form-check-label" for="inlineRadio3">Expert</label>
+                  </div>
+            </div>
+
+            
+
+            <div class="mt-4"> 
+                <x-jet-label for="name" value="{{ __('About yourself') }}" />
+                <textarea id="about" class="block mt-1 w-full border" rows="8" type="text" name="about" :value="old('about')" required autofocus autocomplete="about"></textarea>
+            </div> --}}
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
